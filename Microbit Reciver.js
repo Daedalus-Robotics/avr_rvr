609a7c1bb81c98151e9b8afeeba927de3f643238
owaisoumera <139817035+owaisoumera@users.noreply.github.com>
2024-11-08 08:23:15 -0600

    Update Microbit Reciver.js

--- Microbit Reciver.js
+++ Microbit Reciver.js
@@ -1,14 +1,7 @@
-input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P0, 126)
-    pins.digitalWritePin(DigitalPin.P1, 90)
-})
 radio.onReceivedValue(function (name, value) {
     if (name == "dir") {
         pins.servoWritePin(AnalogPin.P0, value)
     }
-    if (name == "turn") {
-        pins.servoWritePin(AnalogPin.P1, value)
-    }
 })
-radio.setGroup(1)
+radio.setGroup(52)
 basic.showIcon(IconNames.Happy)
