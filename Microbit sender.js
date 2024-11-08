708ee7f68d868861e64653f9ae4edda6216b68f4
owaisoumera <139817035+owaisoumera@users.noreply.github.com>
2024-11-08 08:21:00 -0600

    Update Microbit sender.js

--- Microbit sender.js
+++ Microbit sender.js
@@ -1,12 +1,25 @@
 input.onButtonPressed(Button.A, function () {
+    Send()
+})
+function Send () {
     if (dir == 90) {
-        dir = 180
+        dir = 360
     } else {
         dir = 90
     }
     radio.sendValue("dir", dir)
-})
+}
 let dir = 0
 basic.showIcon(IconNames.Happy)
-radio.setGroup(1)
 dir = 0
+radio.setGroup(1)
+serial.redirect(
+SerialPin.USB_TX,
+SerialPin.USB_RX,
+BaudRate.BaudRate115200
+)
+basic.forever(function () {
+    if (serial.readString() == "move") {
+        Send()
+    }
+})
